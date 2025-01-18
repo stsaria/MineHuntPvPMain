a0bdf743dc1ee3b7fec8d9cd53c877942b0f9a55
stsaria <solothunder.autoer@gmail.com>
2025-01-18 20:13:25 +0900

    h48rh39

--- src/main/java/si/f5/manhuntearth/manhuntearthmain/AlliesLocationsActionBar.java
+++ src/main/java/si/f5/manhuntearth/manhuntearthmain/AlliesLocationsActionBar.java
@@ -3,6 +3,7 @@ package si.f5.manhuntearth.manhuntearthmain;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Location;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import si.f5.manhuntearth.manhuntearthmain.roles.GameTeam;
 import si.f5.manhuntearth.manhuntearthmain.roles.HunterTeam;
@@ -17,16 +18,25 @@ public class AlliesLocationsActionBar {
             }
             StringBuilder locationsStringBuilder = new StringBuilder();
             locationsStringBuilder.append("§a味方の位置§r ");
+            OfflinePlayer offlinePlayer;
             Player player;
             Location location;
             for (GamePlayer gamePlayer : team.GetGamePlayers()){
-                player = (Player) gamePlayer.getBukkitPlayer();
+                offlinePlayer = gamePlayer.getBukkitPlayer();
+                if (!offlinePlayer.isOnline()){
+                    continue;
+                }
+                player = offlinePlayer.getPlayer();
                 location = player.getLocation();
                 locationsStringBuilder.append(player.getName()).append(":").append(location.getBlockX()).append(", ").append(location.getBlockY()).append(", ").append(location.getBlockZ()).append(" / ");
             }
             locationsStringBuilder.delete(locationsStringBuilder.length()-3, locationsStringBuilder.length()-1);
             for (GamePlayer gamePlayer : team.GetGamePlayers()){
-                player = (Player) gamePlayer.getBukkitPlayer();
+                offlinePlayer = gamePlayer.getBukkitPlayer();
+                if (!offlinePlayer.isOnline()){
+                    continue;
+                }
+                player = offlinePlayer.getPlayer();
                 player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(locationsStringBuilder.toString().replace(" "+player.getName()+":", " §e"+player.getName()+"§r:")));
             }
         }
